d22b0a0aa097bff60a9968f2030a8cfcfb6b30fa
nafniko <naffer@hotmail.fr>
2024-11-12 14:45:34 +0100

    maj

--- lib/pdo.php
+++ lib/pdo.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/config.php';
 //     die('Erreur MySQL : ' . $e->getMessage());
 // }
 try {
-    $pdo = new PDO("mysql:dbname=sto2g9dado760ufv;host=uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;charset=utf8mb4","i9j7we0uegcy6a5g","i9j7we0uegcy6a5g");
+    $pdo = new PDO("mysql:dbname=sto2g9dado760ufv;host=uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;charset=utf8mb4","h3wn8n8g66i1rour","i9j7we0uegcy6a5g");
    
 } catch(Exception $e) {
     die('Erreur MySQL : ' . $e->getMessage());
