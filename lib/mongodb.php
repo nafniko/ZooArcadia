e82502a7f4a20ffe31049318cab367658b1f3505
nafniko <naffer@hotmail.fr>
2024-11-13 10:56:03 +0100

    maj

--- lib/mongodb.php
+++ lib/mongodb.php
@@ -1,5 +1,5 @@
 <?php  
-// require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 
 // use Symfony\Component\Security\Csrf\CsrfTokenManager;
@@ -78,4 +78,19 @@
 // }
 
 
-
+use Exception;
+use MongoDB\Client;
+use MongoDB\Driver\ServerApi;
+// Replace the placeholder with your Atlas connection string
+$uri = 'mongodb+srv://zoo:Azerty11@cluster0.njatc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Specify Stable API version 1
+$apiVersion = new ServerApi(ServerApi::V1);
+// Create a new client and connect to the server
+$client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+try {
+    // Send a ping to confirm a successful connection
+    $client->selectDatabase('admin')->command(['ping' => 1]);
+    echo "Pinged your deployment. You successfully connected to MongoDB!\n";
+} catch (Exception $e) {
+    printf($e->getMessage());
+}
