e57584dee9abbf17055a73dc920bb12faf44fc47
nafniko <naffer@hotmail.fr>
2024-11-13 16:16:06 +0100

    maj

--- lib/pdo.php
+++ lib/pdo.php
@@ -17,13 +17,20 @@ require_once __DIR__ . '/config.php';
 //     die('Erreur MySQL : ' . $e->getMessage());
 // }
 
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_NAME');
+// $host = getenv('DB_HOST');
+// $user = getenv('DB_USER');
+// $password = getenv('DB_PASSWORD');
+// $database = getenv('DB_NAME');
+
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
+// } catch(Exception $e) {
+//     die('Erreur MySQL : ' . $e->getMessage());
+// }
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$database;charset=utf8mb4", $user, $password);
+    $pdo = new PDO("mysql:dbname=sto2g9dado760ufv;host=uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;charset=utf8mb4","h3wn8n8g66i1rour","i9j7we0uegcy6a5g");
+   
 } catch(Exception $e) {
     die('Erreur MySQL : ' . $e->getMessage());
 }
