0a984b6b23f6c97ec568389b6df8798f5435543f
nafniko <naffer@hotmail.fr>
2024-11-22 00:00:52 +0100

    maj

--- lib/config.php
+++ lib/config.php
@@ -2,14 +2,14 @@
  require_once __DIR__ . '/../vendor/autoload.php';
 
 
-header("X-Frame-Options: SAMEORIGIN");
-header("Content-Security-Policy: frame-ancestors 'self';");
-header("Content-Security-Policy: default-src 'self'; style-src 'self' https://fonts.googleapis.com; script-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://fonts.gstatic.com data:");
-header('Content-Type: application/json; charset=utf-8');
-header("Content-Type: text/html; charset=UTF-8");
-header("X-Content-Type-Options: nosniff");
+// header("X-Frame-Options: SAMEORIGIN");
+// header("Content-Security-Policy: frame-ancestors 'self';");
+// header("Content-Security-Policy: default-src 'self'; style-src 'self' https://fonts.googleapis.com; script-src 'self'; font-src 'self' https://fonts.gstatic.com; img-src 'self' https://fonts.gstatic.com data:");
+// header('Content-Type: application/json; charset=utf-8');
+// header("Content-Type: text/html; charset=UTF-8");
+// header("X-Content-Type-Options: nosniff");
 
-header_remove("X-Powered-By");
+// header_remove("X-Powered-By");
 
 
 define("STMP_PASSWORD", "rjUImBqN7QMgXT5P");
